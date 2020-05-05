724851a6db29758b3e7421e2449adf100e935136
luyj0812 <luyj0812@gmail.com>
2020-05-06 01:57:55 +1200

    Create standardize(self,X) function

--- knn_classifier.py
+++ knn_classifier.py
@@ -1,9 +1,11 @@
 from skmultiflow.lazy import KNNClassifier
 from skmultiflow.data import SEAGenerator
 from skmultiflow.utils.utils import *
+from sklearn import preprocessing   
 
 
 class MyKNNClassifier(KNNClassifier):
+    
     def __init__(self, 
                  n_neighbors=5, 
                  max_window_size=1000, 
@@ -15,6 +17,20 @@ class MyKNNClassifier(KNNClassifier):
                          leaf_size=leaf_size, 
                          metric=metric)
         self.weighted_vote = weighted_vote
+    '''
+    def __init__(self, 
+                 n_neighbors=5, 
+                 max_window_size=1000, 
+                 leaf_size=30, 
+                 metric='euclidean', 
+                 standardize=False):
+        super().__init__(n_neighbors=n_neighbors, 
+                         max_window_size=max_window_size, 
+                         leaf_size=leaf_size, 
+                         metric=metric)
+        self.standardize = standardize
+    '''
+    
         
     def predict_proba(self,X):
         r, c = get_dimensions(X)
@@ -32,11 +48,20 @@ class MyKNNClassifier(KNNClassifier):
         for i in range(r):
             votes = [0.0 for _ in range(int(max(self.classes) + 1))]
             for index in new_ind[i]:
-                votes[int(self.data_window.targets_buffer[index])] += 1. / float(dist_list[i][count])
-                #votes[int(self.data_window.targets_buffer[index])] += 1. / len(new_ind[i])
+                # Calculate votes by adding 1/distance
+                #votes[int(self.data_window.targets_buffer[index])] += 1. / float(dist_list[i][count])
+                votes[int(self.data_window.targets_buffer[index])] += 1. / len(new_ind[i])
                 count = count + 1
             proba.append(votes)
         return np.asarray(proba)
+    
+    def standardize(self,X):
+        X = X.astype(float)
+        X_mean = X.mean(axis = 0)
+        X_std = X.std()
+        standardization = (X-X_mean) / X_std
+        return standardization
+        
 
 # Setting up the stream
 stream = SEAGenerator(random_state=1, noise_percentage=.1)
@@ -45,14 +70,13 @@ my_knn = MyKNNClassifier(n_neighbors=8, max_window_size=2000, leaf_size=40, weig
 # Keep track of sample count and correct prediction count
 n_samples = 0
 corrects = 0
-while n_samples < 5000:
+while n_samples < 20:
     X, y = stream.next_sample()
-    my_pred = my_knn.predict(X)
-    #my_pred = knn.predict(X)
+    X_standard = my_knn.standardize(X)
+    my_pred = my_knn.predict(X_standard)
     if y[0] == my_pred[0]:
         corrects += 1
-    #knn = knn.partial_fit(X, y)
-    my_knn = my_knn.partial_fit(X,y)
+    my_knn = my_knn.partial_fit(X_standard,y)
     n_samples += 1
 # Displaying results
 #print('KNNClassifier usage example')
